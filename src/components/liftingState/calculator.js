845bea574dee0b1dc50c3f32b7693bdb1ec77fe7
Nestor Diaz <nestor.loaiza@mulesoft.com>
2017-09-29 11:45:07 -0500

    Lifting state up 2

--- src/components/liftingState/calculator.js
+++ src/components/liftingState/calculator.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import BoilerVerdict from './boilerVerdict';
+import TemperatureInput from './temperatureInput';
 
 export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {temperature: ''};
-  }
-
-  handleChange(e) {
-    this.setState({temperature: e.target.value});
-  }
-
   render() {
-    const temperature = this.state.temperature;
     return (
-      <fieldset>
-        <legend>Enter temperature in Celsius</legend>
-        <input value={temperature} onChange={this.handleChange} />
-        <BoilerVerdict celsius={parseFloat(temperature)} />
-      </fieldset>
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
     );
   }
 }
